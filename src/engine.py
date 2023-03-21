0b9386778f341a215dd1b75842fac7dcd6c9b214
Victor <victortolsagerolesen1@gmail.com>
2023-03-21 14:41:05 +0100

    create train loop

--- src/engine.py
+++ src/engine.py
@@ -1,4 +1,6 @@
 import torch
+import tqdm
+import torchmetrics
 from torch import nn
 from typing import Optional
 
@@ -21,7 +23,7 @@ class Trainer:
     def __init__(
         self,
         train_dataloader: torch.utils.data.DataLoader,
-        val_dataloader: Optional[torch.util.data.DataLoader],
+        val_dataloader: Optional[torch.utils.data.DataLoader],
         model: torch.nn.Module,
         epochs: int = 10,
         learning_rate: float = 0.06,
@@ -35,9 +37,32 @@ class Trainer:
         self.epochs = epochs
         self.learning_rate = learning_rate
         self.weight_decay = weight_decay
+        self.optimizer = torch.optim.SGD(
+            model.parameters(), learning_rate, momentum=0.9, weight_decay=weight_decay
+        )
+        self.avg_train_loss = torchmetrics.MeanMetric()
 
     def train_epoch(self):
         for aug1, aug2, _, _ in self.train_dataloader:
             aug1 = aug1.to(self.device)
             aug2 = aug2.to(self.device)
             model_outputs = self.model(aug1, aug2)
+            loss = criterion(model_outputs)
+
+            # optimization
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            loss = loss.detach().cpu()
+            self.avg_train_loss.update(loss)
+
+    def train(self):
+        for epoch in tqdm.trange(self.epochs):
+            self.avg_train_loss.reset()
+            self.train_epoch()
+            val_acc = self.validation()
+            avg_train_loss = self.avg_train_loss.compute()
+            print(f"Epoch: {epoch}")
+            print(f"Average train loss: {avg_train_loss}")
+            print(f"Validation accuracy: {val_acc}")
---
+++ train.py
@@ -0,0 +1,36 @@
+from src import data, model, engine
+import torchvision
+import torch
+
+
+def train():
+    # get datasets
+    train_ds = data.get_cifar10_dataset(train=True)
+    val_ds = data.get_cifar10_dataset(train=False)
+
+    cifar10_train_transforms = torchvision.transforms.Compose(
+        data.cifar10_augmentations
+    )
+    cifar10_val_transforms = torchvision.transforms.Compose(
+        data.cifar10_standard_transforms
+    )
+    train_ds = data.SimSiamDataset(train_ds, cifar10_train_transforms)
+    val_ds = data.AugmentedDataset(val_ds, cifar10_val_transforms)
+
+    # create dataloaders
+    train_dl = torch.utils.data.DataLoader(train_ds, batch_size=512)
+    val_dl = torch.utils.data.DataLoader(val_ds, batch_size=512)
+
+    simsiam = model.SimSiam()
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    simsiam.to(device)
+    trainer = engine.Trainer(
+        train_dl, val_dl, simsiam, epochs=100, learning_rate=0.06, device=device
+    )
+    trainer.train()
+
+
+if __name__ == "__main__":
+    train()
